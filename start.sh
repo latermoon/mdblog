3564dda77607718f93173e37bde2e4d0204787e8
Latermoon <lptmoon@gmail.com>
2015-03-30 10:57:22 +0800

    resolve conflicts

---
+++ start.sh
@@ -0,0 +1,4 @@
+
+export GOPATH=$GOPATH:$(pwd)
+
+go run src/mdblog.go /Users/latermoon/Downloads/GitHub/Sites/blog.latermoon.me
--- xiaowu.sh
+++ xiaowu.sh
@@ -1,2 +1,4 @@
+
 export GOPATH=$GOPATH:$(pwd)
+
 go run src/mdblog.go /Users/latermoon/Downloads/GitHub/blog.xiaowu.me
